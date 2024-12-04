59ffdf01a3d59b914dc35acce07efa0eb23cb041
Simon Willison <swillison@gmail.com>
2024-12-03 18:03:24 -0800

    Test for conversations, closes #2

--- tests/test_bedrock.py
+++ tests/test_bedrock.py
@@ -17,3 +17,14 @@ def test_prompt():
     model.key = API_KEY
     response = model.prompt("What is the capital of France?", system="One word answer")
     assert response.text() == "Paris"
+
+
+@pytest.mark.vcr
+def test_conversation():
+    model = llm.get_model("us.amazon.nova-lite-v1:0")
+    model.key = API_KEY
+    conversation = model.conversation()
+    response = conversation.prompt("What is the capital of France?", system="One word answer")
+    assert response.text() == "Paris"
+    response2 = conversation.prompt("Germany?", system="One word answer")
+    assert response2.text() == "Berlin"
